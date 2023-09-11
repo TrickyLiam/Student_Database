203d17bc6fbb87df2264c078a39307281ae9f5cd
Chillsbro <114181571+Chillsbro@users.noreply.github.com>
2023-09-11 15:38:38 -0500

    add base logic to student class for creation of object

--- src/main/java/org/example/DatabaseMain.java
+++ src/main/java/org/example/DatabaseMain.java
@@ -23,7 +23,9 @@ public class DatabaseMain {
             }
             questions[i] = scanner.nextLine();
         }
+
         return questions;
     }
+
 
 }
--- src/main/java/org/example/Student.java
+++ src/main/java/org/example/Student.java
@@ -1,8 +1,23 @@
 package org.example;
 
 
+import java.lang.reflect.Constructor;
+
 public class Student {
     private String Name;
     private String GradeLevel;
 
+    private double Balance;
+
+    public Student(String n, String gl) {
+        Name = n;
+        GradeLevel = gl;
+    }
+
+    public String getName() {
+        return Name;
+    }
+    public String getGradeLevel () {
+        return GradeLevel;
+    }
 }
